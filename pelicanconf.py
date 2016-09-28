0b0ad6eb254b40a0f63a785b81afcc64a3d352eb
Gints Murans <gm@gm.lv>
2016-09-28 23:21:36 +0300

    Theme and other stuff

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+PATH = 'content'
+
 AUTHOR = 'Gints Murāns'
 SITENAME = 'Dzīvoklis'
 SITEURL = ''
 
-PATH = 'content'
-
 TIMEZONE = 'Europe/Riga'
-
 DEFAULT_LANG = 'lv'
+
+DEFAULT_PAGINATION = 10
+SUMMARY_MAX_LENGTH = 180
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,18 +22,32 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 10
+SOCIAL = (
+    ('facebook', 'https://www.facebook.com/gintsmurans'),
+    ('instagram', 'https://www.instagram.com/gintsmurans/', 'instagram'),
+    ('github', 'http://github.com/gintsmurans')
+)
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = 'notmyidea'
+# THEME stuff
+THEME = 'theme'
+BOOTSTRAP_THEME = 'united'
+
+DISPLAY_CATEGORIES_ON_MENU = False
+PYGMENTS_STYLE = 'solarizeddark'
+DISPLAY_BREADCRUMBS = False
+FAVICON = ''
+DISPLAY_ARTICLE_INFO_ON_INDEX = False
+
+DISPLAY_TAGS_ON_SIDEBAR = False
+DISPLAY_CATEGORIES_ON_SIDEBAR = False
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = False
+RECENT_POST_COUNT = 5
+
+DISQUS_NO_ID = True
+CC_LICENSE = 'CC-BY'
